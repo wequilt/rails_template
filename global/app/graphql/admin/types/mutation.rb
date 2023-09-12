45f43f89aeeee8c9768db2445c480f4c26747d4e
Gregory Jones <greg@wequilt.com>
2023-09-11 20:17:43 -0400

    Add placeholder fields for mutations

--- global/app/graphql/admin/types/mutation.rb
+++ global/app/graphql/admin/types/mutation.rb
@@ -4,6 +4,8 @@ module Admin
   module Types
     class Mutation < Shared::Types::Base
       description 'The base mutation type for this schema'
+
+      field :remove_me, String, 'Remove once other fields are added', inaccessible: true, shareable: true
     end
   end
 end
--- global/app/graphql/types/mutation.rb
+++ global/app/graphql/types/mutation.rb
@@ -3,5 +3,7 @@
 module Types
   class Mutation < Shared::Types::Base
     description 'The mutation root of this schema'
+
+    field :remove_me, String, 'Remove once other fields are added', inaccessible: true, shareable: true
   end
 end
